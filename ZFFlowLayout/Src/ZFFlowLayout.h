73b4fa003dc8c7376076b457b1badeb0fc9cec71
ZHOUFEI <1174977961@qq.com>
2017-05-23 21:55:34 +0800

    添加自定义创建布局实例的方法

--- ZFFlowLayout/Src/ZFFlowLayout.h
+++ ZFFlowLayout/Src/ZFFlowLayout.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UICollectionViewFlowLayout.h>
 #import "ZFFlowLayoutMacro.h"
+#import "ZFFlowLayoutProtocol.h"
 
 //流水布局类型
 typedef enum : NSUInteger {
@@ -25,5 +26,19 @@ typedef enum : NSUInteger {
  *  @return 布局实例
  */
 + (UICollectionViewFlowLayout *)flowLayoutWithFlowLayoutType:(FlowLayoutType)flowLayoutType;
+
+
+/*!
+ *  @author zhoufei
+ *
+ *  @brief 自定义布局实例：根据传入不同的流失布局类型，item距离四周距离，section距离四周距离 自定义布局实例
+ *  @param flowLayoutType 流水布局类型
+ *  @param itemEdgeInsets 第一个item距离四周的距离
+ *  @param sectionEdgeInsets section距离四周的距离
+ *  @return 布局实例
+ */
++ (UICollectionViewFlowLayout<ZFFlowLayoutProtocol> *)flowLayoutWithFlowLayoutType:(FlowLayoutType)flowLayoutType
+                                                                    ItemEdgeInsets:(FlowLayoutItemEdgeInsets)itemEdgeInsets
+                                                                 sectionEdgeInsets:(FlowLayoutSectionEdgeInsets)sectionEdgeInsets;
 
 @end
--- ZFFlowLayout/Src/ZFFlowLayout.m
+++ ZFFlowLayout/Src/ZFFlowLayout.m
@@ -8,7 +8,6 @@
 //  简单工厂模式，根据不同的布局类型，生产出不同的布局方式。
 
 #import "ZFFlowLayout.h"
-#import "ZFFlowLayoutMacro.h"
 #import "LeftAlignedFlowLayout.h"
 #import "RightAlignedFlowLayout.h"
 
@@ -23,21 +22,41 @@
  */
 + (UICollectionViewFlowLayout *)flowLayoutWithFlowLayoutType:(FlowLayoutType)flowLayoutType {
 
+    
     UICollectionViewFlowLayout <ZFFlowLayoutProtocol> *  flowLayout = nil;
+    flowLayout = [self flowLayoutWithFlowLayoutType:flowLayoutType
+                                     ItemEdgeInsets:(FlowLayoutItemEdgeInsets){20,10,20,20}
+                                  sectionEdgeInsets:(FlowLayoutSectionEdgeInsets){20,10,20,10}];
+    
+    return flowLayout;
+}
+
+
+/*!
+ *  @author zhoufei
+ *
+ *  @brief 自定义布局实例：根据传入不同的流失布局类型，item距离四周距离，section距离四周距离 自定义布局实例
+ *  @param flowLayoutType 流水布局类型
+ *  @param itemEdgeInsets 第一个item距离四周的距离
+ *  @param sectionEdgeInsets section距离四周的距离
+ *  @return 布局实例
+ */
++ (UICollectionViewFlowLayout<ZFFlowLayoutProtocol> *)flowLayoutWithFlowLayoutType:(FlowLayoutType)flowLayoutType
+                                                                    ItemEdgeInsets:(FlowLayoutItemEdgeInsets)itemEdgeInsets
+                                                                 sectionEdgeInsets:(FlowLayoutSectionEdgeInsets)sectionEdgeInsets
+{
+    UICollectionViewFlowLayout <ZFFlowLayoutProtocol> *  flowLayout = nil;
+    
     if (flowLayoutType == FlowLayoutType_leftAlign) {
         
-        flowLayout = [LeftAlignedFlowLayout new];
-        [flowLayout configFlowLayoutWithFlowLayoutItemEdgeInsets:(FlowLayoutItemEdgeInsets){20,10,20,20}
-                                         sectionEdgeInsets:(FlowLayoutSectionEdgeInsets){20,10,20,10}];
-
+        flowLayout =  [LeftAlignedFlowLayout new];
     } else if (flowLayoutType == FlowLayoutType_rightAlign) {
         
-        flowLayout = [RightAlignedFlowLayout new];
-        [flowLayout configFlowLayoutWithFlowLayoutItemEdgeInsets:(FlowLayoutItemEdgeInsets){20,10,20,20}
-                                               sectionEdgeInsets:(FlowLayoutSectionEdgeInsets){20,10,20,10}];
-
+        flowLayout =  [RightAlignedFlowLayout new];
     }
-    
+
+    [flowLayout configFlowLayoutWithFlowLayoutItemEdgeInsets:itemEdgeInsets
+                                           sectionEdgeInsets:sectionEdgeInsets];
     
     return flowLayout;
 }
