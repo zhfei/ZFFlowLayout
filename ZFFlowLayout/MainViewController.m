c1269509ecd32a398f6f8751a818f00a78e63ac5
zhoufei <zf@xkeshi.com>
2017-09-06 16:25:25 +0800

    other:删除无用代码

--- ZFFlowLayout/MainViewController.m
+++ ZFFlowLayout/MainViewController.m
@@ -26,21 +26,6 @@
     self.view.layer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"timg3.jpeg"].CGImage);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)leftAlignBtnAction:(id)sender {
     
     UICollectionViewFlowLayout * flowLayout = [ZFFlowLayout flowLayoutWithFlowLayoutType:FlowLayoutType_leftAlign];
